1a361ab52192d41e1f7e885556474aab04b5c68b
mastertenshi <angelmitev@protonmail.com>
2021-02-25 04:05:26 +0100

    Refactor bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -5,26 +5,19 @@ def bubble_sort(arr)
 
     arr.each_index do |k|
       break unless k < limit
-
       if arr[k] > arr[k + 1]
-        swap(arr, k, k + 1)
+        arr[k], arr[k + 1] = arr[k + 1], arr[k]
         sorted = false
       end
     end
 
-    if sorted
-      break
-    end
+    break if sorted
   end
-
   arr
 end
 
-def swap(arr, i, k)
-  arr[i], arr[k] = arr[k], arr[i]
-end
 
-data = [2, 6, 2, 1, 5, 8, 6, 0, 2, 8, 9]
+data = [10, 2, 6, 2, 1, 5, 8, 6, 0, 2, 8, 9]
 
 p bubble_sort(data)
-# => [0, 1, 2, 2, 2, 5, 6, 6, 8, 8, 9]
+# => [0, 1, 2, 2, 2, 5, 6, 6, 8, 8, 9, 10]
