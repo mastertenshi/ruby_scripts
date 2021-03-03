06e688d5d660b4091928cdbf67ab2b957ddf4e94
mastertenshi <angelmitev@protonmail.com>
2021-03-03 19:54:06 +0100

    Add #draw_board (tic_tiac_toe.rb)

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,61 +1,60 @@
 module TicTacToe
   class Board
-    attr_accessor :grid
-
-    def initialize(size_x = 3, size_y = 3)
+    def initialize
       @grid = []
-      @grid_hash = {}
-      set_grids(size_x, size_y)
+      @grid_number_map = {}
+      set_grids
     end
 
-    def print
-      p @grid
-      p @grid_hash
+    def draw_board
+      system 'clear'
+      puts "\v"
+
+      # "\e[34m"  # set format (blue foreground)
+      # "\e[0m"   # clear format
+
+      @grid.each_with_index do |array, index|
+        string = "\t"
+
+        array.each do |n|
+          string += " \e[34m#{n}\e[0m "
+          string += '|' unless (n % 3).zero?
+        end
+        puts string[0, string.length]
+        puts "\t---+---+---" unless index == 2
+      end
+      puts "\v"
+
     end
 
     private
 
-    def set_grids(x, y)
+    def set_grids
       num = 1
 
-      for i in 0...x
+      (0..2).each do |i|
         @grid.push([])
-        for k in 0...y
+
+        (0..2).each do |k|
           @grid[i].push(num)
-          @grid_hash[num] = [i, k]
+          @grid_number_map[num] = [i, k]
           num += 1
         end
       end
     end
   end
 
-  class Player
-    def initialize(name = 'Player', symbol = '')
-      @name = name
-      @symbol = symbol
-    end
-  end
+
+  # class Player
+  #   def initialize(name = 'Player', symbol = '')
+  #     @name = name
+  #     @symbol = symbol
+  #   end
+  # end
 end
 
-system 'clear'
-puts " X | 2 | 3 "
-puts "---+---+---"
-puts " O | 5 | X "
-puts "---+---+---"
-puts " O | X | O "
-puts ""
 board = TicTacToe::Board.new
 
-board.print
+board.draw_board
+
 # p ([1, 5, 3, 7, 2, 5] & [1, 7, 5]).count
-
-number_grid = {}
-
-num = 1
-
-for i in 0..2
-  for k in 0..2
-    number_grid[num] = [i, k]
-    num += 1
-  end
-end
