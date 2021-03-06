c4fcec1fd2d00eb3204feaf6cd20b972b26506eb
Angel Mitev <56616191+mastertenshi@users.noreply.github.com>
2021-03-06 18:57:55 +0100

    Move tic-tac-toe-project into separate repository

--- tic_tac_toe_project/play.rb
+++
@@ -1,3 +0,0 @@
-require_relative 'tic_tac_toe'
-
-TicTacToe::Game.new.play
--- tic_tac_toe_project/tic_tac_toe.rb
+++
@@ -1,6 +0,0 @@
-require_relative 'tic_tac_toe/board'
-require_relative 'tic_tac_toe/player'
-require_relative 'tic_tac_toe/game'
-require_relative 'tic_tac_toe/display'
-
-require 'colorize'
